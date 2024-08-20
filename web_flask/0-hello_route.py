8570f15766c854c4628b3bce1917eef86d518605
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-08-20 19:43:41 +0100

    create web_flask pypackage add 0-hello_route.py add __init__.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script start a basic flask app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a simple messgae response
+    """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
